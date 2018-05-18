44119eeb363d703fabfaa2a877d00c20f4e2648f
LeeVaughn <dlvprogramming@gmail.com>
2018-05-17 20:59:15 -0700

    added basic functionality for api call.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-// api url = https://randomuser.me/api/?results=12&nat=us&exc=gender,registered,phone,id,nat
+$(document).ready(function () {
+
+const url = "https://randomuser.me/api/?results=12&nat=us&exc=gender,registered,phone,id,nat&callback=?";
+
+$.getJSON(url, function (data) {
+    // displays first name of first object in the results array
+    console.log(data.results[0].name.first);
+
+});
+
+
+});
